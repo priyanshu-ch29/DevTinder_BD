3424e91c143e955f0f87a7b0a9dbdd229900d21a
priyanshu-ch29 <91934414+priyanshu-ch29@users.noreply.github.com>
2026-02-03 18:14:43 +0530

    env changes

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        const con = await mongoose.connect(process.env.DB_URL, {
+        const con = await mongoose.connect(process.env.DB_PROD_URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         })
